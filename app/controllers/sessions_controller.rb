a5d23cc02e591c994314c6b888e276b71b4ed50d
Emi Ko <emi8emi@gmail.com>
2018-07-26 23:39:09 -0700

    refactor sessions login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,26 @@
 class SessionsController < ApplicationController
+
   def create
-    if @user = User.from_omniauth(request.env["omniauth.auth"])
+  auth = request.env["omniauth.auth"]
+  if auth
+    sign_in_with_auth(auth)
+  else
+    sign_in_with_password
+  end
+end
+  def sign_in_with_auth(auth)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
   	session[:user_id] = @user.id
     redirect_to user_path(@user)
-   elsif  @user = User.find_by(name: params[:user][:name]) && @user.authenticate(params[:user][:password])
+  end
+  def sign_in_with_password
+     if @user = User.find_by(name: params[:user][:name])
+       @user.authenticate(params[:user][:password])
        session[:user_id] = @user.id
        redirect_to user_path(@user)
      else
        redirect_to '/signin'
      end
-   end
+    end
+
   end
