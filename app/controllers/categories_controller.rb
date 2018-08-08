000cdf5ec54510ce4c3e3c2361bb12562311b4db
Emi Ko <emi8emi@gmail.com>
2018-08-08 12:54:31 -0700

    add search by item name in categories_path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,9 +16,23 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    if params[:search] && params[:search] != ""
+      @items = Item.where('name LIKE ?', "%#{params[:search]}%")
+    else
       @categories = Category.all
+    end
   end
 
+  def self.search_items(word)
+    if word && !word.blank?
+      @users = User.where(city: word.upcase)
+      @users.map do |user|
+      user.items
+      end.flatten
+    else
+      Item.all
+    end
+  end
   private
   def category_params
     params.require(:category).permit(:name, :description)
